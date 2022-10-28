a362308e52ea782087cd8a31869c120af41584b6
Asimi Anas <asimianas@outlook.com>
2022-10-28 20:03:33 +0100

    finish the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -16,115 +16,28 @@ export default function App() {
       <Projects />
       <Contact />
       <Footer />
-      <div className="div1">
-        <div className="div2">
-          <div className="div3">
-            <div className="div4"></div>
-          </div>
-        </div>
-      </div>
-      <div>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-      <p>hello from other side hehe</p>
-
-      </div>
+      {/* for test */}
+      <div className="text-dummy">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil laudantium sit neque cumque dignissimos, similique eveniet quod maxime dolores, obcaecati soluta explicabo quam, quas quasi assumenda deleniti totam corrupti ipsum?
+        Itaque cumque harum similique consequatur omnis quaerat voluptas tempora autem veritatis hic. Laudantium tempore culpa dignissimos commodi ducimus, vel quam quaerat fugiat ipsam adipisci qui optio esse ea nulla quas.
+        Necessitatibus, numquam. Officia, cum eos omnis iusto perferendis quibusdam nostrum doloribus amet blanditiis quas saepe eius harum, illum sit eveniet praesentium at velit possimus distinctio dolores corporis nobis deleniti. Ea.
+        Magni minima, et dolores possimus voluptates veniam ut eum corporis itaque impedit necessitatibus maxime commodi nemo harum, unde perferendis voluptatem ipsa tenetur. Illum aperiam culpa optio libero harum voluptatibus beatae.
+        Amet, aspernatur mollitia. Labore molestiae non sit quae officiis? Consectetur est nihil illum adipisci dolore. Minus molestias nobis voluptatem, dolor placeat accusamus? Omnis cum veritatis, quam vero autem enim odio?
+        Molestias voluptas ullam nemo. Amet neque enim pariatur voluptatum cum. Itaque accusantium, voluptatum ratione cupiditate fugiat temporibus accusamus officiis ducimus iure blanditiis, cumque odit provident illo dolore aspernatur. Ad, nostrum.
+        Ab quaerat aut reprehenderit itaque blanditiis illo, voluptatibus, perferendis tempore quis vel architecto tenetur nulla sint, necessitatibus obcaecati deleniti facilis nobis maxime cum optio culpa eligendi voluptate eveniet soluta! Voluptatibus.
+        Saepe quia cum doloribus aperiam ipsum reprehenderit dicta nostrum, tempora neque ea eveniet debitis eos quisquam optio numquam. Sint quod in repellat at eos praesentium tempora, quam similique voluptatum eveniet!
+        Hic ipsum nam similique sunt laborum. Pariatur, ex. Veritatis, consectetur nobis earum hic soluta aliquam at neque id fuga eaque accusamus sint atque consequatur dicta a alias placeat ut doloribus.
+        Architecto sint deleniti quisquam a reiciendis necessitatibus sunt quia et dicta at doloribus odit omnis laborum, repellat, impedit natus libero sit perferendis dolores cupiditate. Maxime molestias officia accusantium laborum nisi.
+        Vero cupiditate temporibus mollitia inventore soluta alias, cumque nobis accusamus eius iure, perspiciatis ducimus fugit officia omnis repellendus impedit velit doloribus ratione? Ratione iste aliquid velit eligendi incidunt adipisci deleniti.
+        Exercitationem inventore recusandae quas, ad impedit cum nostrum facere aspernatur nesciunt nulla earum, amet placeat eaque natus dolores distinctio aliquam voluptas, cumque unde repudiandae voluptatem nihil quae? Ratione, accusamus quisquam?
+        Facilis nobis doloremque ipsum ut dicta et vel, impedit illum itaque. Neque minus voluptatibus laudantium vitae enim saepe explicabo modi quos officiis quam sequi, rerum, molestias, earum numquam natus harum?
+        Illo laudantium nulla quo voluptatibus dolore! Alias excepturi, in perferendis placeat laboriosam ipsam eveniet aliquid nesciunt modi rerum! Tempore quibusdam asperiores consequatur sed aliquam, deleniti itaque perspiciatis repudiandae ut laboriosam.
+        Maxime illum quisquam repudiandae quas mollitia perferendis nostrum culpa laborum beatae iusto, exercitationem inventore dignissimos obcaecati delectus reiciendis impedit voluptatum? Necessitatibus sit quas deserunt voluptatibus? Pariatur eaque maiores corporis laudantium!
+        Magni corporis animi sequi aperiam expedita nam mollitia voluptatibus iusto, fugit, et modi error quisquam similique quae. Ad explicabo quas magni corporis ea veniam doloremque obcaecati, ex accusamus molestiae culpa.
+        Non, nemo tempore? Laborum perspiciatis voluptate explicabo accusantium, sed, laboriosam ab quia quod voluptas dolor eveniet? Tempore dolor adipisci dicta ad. Officia nemo repudiandae, magnam eligendi est numquam vitae perferendis?
+        Harum exercitationem qui, neque reprehenderit tempora, magni quaerat, eius mollitia autem vero voluptates corrupti quas. Alias nisi esse doloribus veritatis ipsa libero rerum, culpa perspiciatis maxime voluptate, praesentium dolorem nostrum!
+        Ipsa corporis enim ut saepe veritatis mollitia sapiente laborum at debitis aliquid. Asperiores impedit quis dolorum, aperiam modi consectetur porro possimus dolore natus saepe magni deleniti laboriosam rerum maiores numquam.
+        Nulla blanditiis, saepe, optio maxime quia, illo sed tenetur voluptates voluptatem reprehenderit tempore voluptatum eligendi. Sunt dicta consectetur maxime neque, obcaecati beatae assumenda aliquid adipisci quod ratione? Non, quia ducimus.        </div>
     </>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import * as Unicons from '@iconscout/react-unicons';
 
-
 export default function home() {
   return (
     <main id='home'>
@@ -9,10 +8,16 @@ export default function home() {
         <h2 className="hero__main-hey">HEY THERE,</h2>
         <h1 className="hero__main-title">I'm Anas Asimi.</h1>
         <p className="hero__main-whomi">Self-taught front-end developer with lot of ideas and passion.</p>
-        <button className="button-resume">Download Resume</button>
-        <Unicons.UilAngleDoubleDown className="button-scroll"/>
+        <button className="button-resume" onClick={() => { alert('will be supported soon.') }}>Download Resume</button>
+        <Unicons.UilAngleDoubleDown className="button-scroll" />
       </div>
-      <div className="social">facebook</div>
+      <div className="home__social">
+        <a href="https://facebook.com/AnasAssimi10/"><img src="src/assets/icons/facebook.svg" alt="facebook" /></a>
+        <a href="https://www.linkedin.com/in/anas-asimi-8241b3200/"><img src="src/assets/icons/linkedin.svg" alt="linkedin" /></a>
+        <a href="https://github.com/anas-asimi"><img src="src/assets/icons/github.svg" alt="github" /></a>
+        <a href="mailto:anas2020cr7@gmail.com"><img src="src/assets/icons/mail.svg" alt="mail" /></a>
+        <a href="tel:+212697748319"><img src="src/assets/icons/phone.svg" alt="phone" /></a>
+      </div>
     </main>
   )
 }
