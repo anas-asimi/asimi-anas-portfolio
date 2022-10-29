ecbeef020bb4857364a79092afaba652ad720f79
Asimi Anas <asimianas@outlook.com>
2022-10-30 00:27:18 +0100

    improve

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,45 @@ import Skills from "./components/skills";
 import Projects from "./components/projects";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    // smooth scroll
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth',
+          top: '-64px'
+        });
+      });
+    });
+    // cursor circles
+    const cursorOuter = document.querySelector('.cursor-outer');
+    const cursorInner = document.querySelector('.cursor-inner');
+    const hoverables = document.querySelectorAll('.hoverable');
+    // Listeners
+    document.addEventListener('mousemove', onMouseMove);
+    for (let i = 0; i < hoverables.length; i++) {
+      hoverables[i].addEventListener('mouseenter', onMouseHover);
+      hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+    }
+    // Move the cursor
+    function onMouseMove(e) {
+      cursorOuter.style.translate = `${e.pageX}px ${e.pageY}px`
+      cursorInner.style.translate = `${e.pageX}px ${e.pageY}px`
+    }
+    // Hover an element
+    function onMouseHover() {
+      cursorOuter.classList.add('cursor-scale')
+    }
+    function onMouseHoverOut() {
+      cursorOuter.classList.remove('cursor-scale')
+    }
+  }, [])
+
   return (
     <>
       <Header />
@@ -18,41 +55,4 @@ export default function App() {
       <Footer />
     </>
   );
-}
-
-window.onload = () => {
-  // smooth scroll
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth',
-        top: '-64px'
-      });
-    });
-  });
-  // cursor circles
-  const cursorOuter = document.querySelector('.cursor-outer');
-  const cursorInner = document.querySelector('.cursor-inner');
-  const hoverables = document.querySelectorAll('.hoverable');
-  // Listeners
-  document.addEventListener('mousemove', onMouseMove);
-  for (let i = 0; i < hoverables.length; i++) {
-    hoverables[i].addEventListener('mouseenter', onMouseHover);
-    hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
-  }
-  // Move the cursor
-  function onMouseMove(e) {
-    cursorOuter.style.translate = `${e.pageX}px ${e.pageY}px`
-    cursorInner.style.translate = `${e.pageX}px ${e.pageY}px`
-  }
-  // Hover an element
-  function onMouseHover() {
-    cursorOuter.classList.add('cursor-scale')
-  }
-  function onMouseHoverOut() {
-    cursorOuter.classList.remove('cursor-scale')
-  }
-
 }
