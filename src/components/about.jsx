c46c4be838cb592c0c3a39a84b88cd9be3199427
Asimi Anas <asimianas@outlook.com>
2022-10-29 02:31:58 +0100

    finish the About section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,10 @@ export default function about() {
   return (
     <div id='about'>
       <Title>About me</Title>
-      <div>about</div>
+      <div className="about__content">
+        <p>Nice to meet You, I'm Anas. I enjoy building dynamic, creative products from start to finish. Focused on developing intuitive experiences that constantly grow and improve. Always shipping.</p>
+        <p>I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.</p>
+      </div>
     </div>
   )
 }
