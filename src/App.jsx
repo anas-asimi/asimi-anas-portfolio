8c0834dccbb567dd10fff2044df0a91eda39c64e
Asimi Anas <asimianas@outlook.com>
2022-10-30 19:03:34 +0000

    improve

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ export default function App() {
     
       // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
       offset: 120, // offset (in px) from the original trigger point
-      delay: 100, // values from 0 to 3000, with step 50ms
+      delay: 0, // values from 0 to 3000, with step 50ms
       duration: 800, // values from 0 to 3000, with step 50ms
       easing: 'ease', // default easing for AOS animations
       once: false, // whether animation should happen only once - while scrolling down
