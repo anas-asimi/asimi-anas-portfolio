4cc721e5d534bba2c54ba90ec9c86504334c5e66
Anas Asimi <anas2020cr7@gmail.com>
2023-03-10 00:33:25 +0100

    fix scroll after refresh

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,20 +4,20 @@ import * as Unicons from '@iconscout/react-unicons';
 export default function home() {
   return (
     <main id='home'>
-      <div className="hero__main">
-        <h2 className="hero__main-hey"  data-aos="slide-up">HEY THERE,</h2>
-        <h1 className="hero__main-title"  data-aos="slide-up">I'm Anas Asimi.</h1>
-        <p className="hero__main-whoami"  data-aos="slide-up">Self-taught front-end developer with lot of ideas and passion.</p>
-        <button className="button-resume hoverable"  data-aos="slide-up" onClick={() => { alert('will be supported soon.') }}>Download Resume</button>
+      <div className="hero__main" data-aos="slide-down">
+        <h2 className="hero__main-hey">HEY THERE,</h2>
+        <h1 className="hero__main-title">I'm Anas Asimi.</h1>
+        <p className="hero__main-whoami">Self-taught front-end developer with lot of ideas and passion.</p>
+        <button className="button-resume hoverable" onClick={() => { alert('will be supported soon.') }}>Download Resume</button>
       </div>
-      <div className="home__social" data-aos="slide-up">
+      <div className="home__social" data-aos="slide-down">
         <a className='hoverable' target="_blank" href="https://facebook.com/AnasAssimi10/"><img src="./assets/social icons/facebook.svg" alt="facebook" /></a>
         <a className='hoverable' target="_blank" href="https://www.linkedin.com/in/anas-asimi-8241b3200/"><img src="./assets/social icons/linkedin.svg" alt="linkedin" /></a>
         <a className='hoverable' target="_blank" href="https://github.com/anas-asimi"><img src="./assets/social icons/github.svg" alt="github" /></a>
         <a className='hoverable' href="mailto:anas2020cr7@gmail.com"><img src="./assets/social icons/mail.svg" alt="mail" /></a>
         <a className='hoverable' href="tel:+212697748319"><img src="./assets/social icons/phone.svg" alt="phone" /></a>
       </div>
-      <Unicons.UilAngleDoubleDown className="button-scroll"  data-aos="slide-up"/>
+      <Unicons.UilAngleDoubleDown className="button-scroll" />
     </main>
   )
 }
