2a549e15660fcf7bb07b60c5ed2e21ad193b53bc
Anas Asimi <anas2020cr7@gmail.com>
2023-05-13 18:28:11 +0100

    remove svg-inject from dependencies and use vite svg plugin instead

--- src/utils/initialize.js
+++ src/utils/initialize.js
@@ -1,17 +1,8 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import SVGInject from '@iconfu/svg-inject/dist/svg-inject.min.js'
 
 
 function initialize() {
-    // SVG injection
-    const symbols = document.querySelectorAll('.symbols__background .container_child img')
-    SVGInject(symbols)
-        .then(() => {
-            // make symbols visible
-            const symbols_container = document.querySelector('#symbols')
-            symbols_container.style.display = 'block'
-        })
     // aos initialize
     AOS.init({
         duration: 500, // values from 0 to 3000, with step 50ms
