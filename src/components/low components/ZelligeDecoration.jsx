23e6a76aa3295ffab7b67b18974930ee466be6d8
Anas Asimi <anas2020cr7@gmail.com>
2023-05-31 19:37:55 +0100

    add parallax scrolling to the Zellige decoration in the background

--- src/components/low components/ZelligeDecoration.jsx
+++ src/components/low components/ZelligeDecoration.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import { ReactComponent as Triangle } from "./background.svg";
 import './ZelligeDecoration.scss'
+import { useRef } from "react";
 
 function ZelligeDecoration() {
+    let zellige = useRef()
+    // changing according to scroll distance
+    useEffect(() => {
+        window.addEventListener("scroll", (e) => {
+            zellige.current.style.translate = `0 ${window.scrollY / 2}px`
+        })
+    }, [])
     return (
-        <div className="zellige-container">
+        <div className="zellige-container" ref={zellige}>
             {[...Array(10)].map((_, i) =>
                 <div key={i} className="container_child">
                     <Triangle />
